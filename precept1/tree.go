587de107a0496f6809a6537c672d018d27366586
Oliver Schwartz <ollieschwartz99@gmail.com>
2019-09-13 14:12:15 -0400

    recursive add function

--- precept1/tree.go
+++ precept1/tree.go
@@ -12,10 +12,23 @@ type Node struct {
 	Val int 
 }
 
+func add(n *Node) int {
+	if n == nil {
+		return 0
+	}
+	return n.Val + add(n.Left) + add(n.Right)
+}
+
 func main() {
 	root := Node{nil, nil, 1}
-	t := Tree{root}
 	root.Left = &Node{nil, nil, 2}
 	root.Right = &Node{nil, nil, 3}
+	root.Left.Left = &Node{nil, nil, 4}
+	root.Left.Right = &Node{nil, nil, 5}
+	root.Right.Left = &Node{nil, nil, 6}
+	root.Right.Right = &Node{nil, nil, 7}
+	fmt.Println(add(&root))
 }
+
+
 
